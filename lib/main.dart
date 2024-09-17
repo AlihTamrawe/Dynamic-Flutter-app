8dfbc36fa3444da1e5fd6022e82bde69cc369ac5
AlihTamrawe <59147506+AlihTamrawe@users.noreply.github.com>
2024-09-17 23:51:55 +0300

    edit color

--- lib/main.dart
+++ lib/main.dart
@@ -13,7 +13,7 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       title: 'Flutter ali first',
       theme: ThemeData(
-        primarySwatch:Colors.blue,
+        primarySwatch:Colors.red, // change color
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
       ),
